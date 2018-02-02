10dc9bd32fd3d5cf1c12527640253d9b1bf36d7d
Yuriy Kartuzov <kyuriy@myseneca.ca>
2018-02-02 00:01:48 -0500

    working test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+let expect = require('chai').expect;
+let chai = require('chai');
+let app = require("../server.js");
+let fs = require('fs');
+
+chai.use(require('chai-http')); // to simulare http request
+
+describe("index request", ()=>{
+    it('Should return html page', () => {
+        return chai.request(app)
+            .get('/')
+            .then((res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+            });
+    })
+});
