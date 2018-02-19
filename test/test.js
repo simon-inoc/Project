0af32b078b49dd24918253dfe68fdaf6cdcbba8f
Abdul Kabia <akkabia@gmail.com>
2018-02-19 12:24:10 -0500

    Added Test
    - Added one more test
    - Fixed server.js to allow testing without crash

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 let expect = require('chai').expect;
-let chai   = require('chai');
-let app    = require("../server.js");
-let fs     = require('fs');
+let chai = require('chai');
+let app = require("../server.js");
+let fs = require('fs');
 
 chai.use(require('chai-http')); // to simulare http request
 
-describe("index request", ()=>{
+// GET - Root
+describe("index request", () => {
     it('Should return html page', () => {
         return chai.request(app)
             .get('/')
@@ -14,4 +15,21 @@ describe("index request", ()=>{
                 expect(res.body).to.be.a('object');
             });
     })
+});
+
+// POST - Root
+describe('API POST request /file', () => {
+    it(`Should resturn status 200. With [
+        "+1 416-568-1010",
+        "+1 905-754-3344"
+    ]`, () => {
+        return chai.request(app)
+            .post('/file')
+            .set('Content-Type', 'text/plain;charset=base64')
+            .attach('file', fs.readFileSync('test/test.txt'), ' ')
+            .then(function (response) {
+                expect(response).to.have.status(200);
+                expect(response.body).to.contain("+1 416-568-1010","+1 905-754-3344");
+            });
+    });
 });
